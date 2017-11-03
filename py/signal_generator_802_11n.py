cf55eada057de690abe856b88235777f19746e8b
Marko Kosunen <marko.kosunen@aalto.fi>
2017-11-03 13:28:50 -0700

    Modify 802n signal generator part and reorganizing functions
    802 signal generation to own class
    Short sequence generator ready. Long sequence needs debugging (tg)
    Multirate functions to the own class

--- py/signal_generator_802_11n.py
+++ py/signal_generator_802_11n.py
@@ -1,5 +1,10 @@
 # signal_generator_802_11n class 
-# Last modification by Marko Kosunen, marko.kosunen@aalto.fi, 31.10.2017 12:02
+# Last modification by Marko Kosunen, marko.kosunen@aalto.fi, 03.11.2017 10:56
+import sys
+sys.path.append ('/home/projects/fader/TheSDK/Entities/refptr/py')
+sys.path.append ('/home/projects/fader/TheSDK/Entities/thesdk/py')
+sys.path.append ('/home/projects/fader/TheSDK/Entities/modem/py')
+sys.path.append ('/home/projects/fader/TheSDK/Entities/multirate/py')
 import numpy as np
 import scipy.signal as sig
 import tempfile
@@ -9,21 +14,29 @@ import time
 
 from refptr import *
 from thesdk import *
-from rtl import *
+
 import modem as mdm #Function definitions
+import multirate as mr
 
-#Simple buffer template
+#+1 added because the ranges do not include the endpoint
+ofdm64dict_withguardband={ 'framelen':64,'data_loc': np.r_[-26:-22+1, -20:-8+1, 
+    -6:-1+1, 1:6+1, 8:20+1, 22:26+1 ], 'pilot_loc' : np.r_[-21, -7, 7, 21], 'CPlen':16}
 
-ofdm64dict_noguardband={ 'framelen':64,'data_loc': np.r_[1:11+1, 13:25+1, 
-    27:39+1, 41:53+1, 55:64+1]-1, 'pilot_loc' : np.r_[-21, -7, 7, 21] + 32, 'CPlen':16}
-
-ofdm64dict_withguardband={ 'framelen':64,'data_loc': np.r_[7:11+1, 13:25+1, 
-    27:32+1, 34:39+1, 41:53+1, 55:59+1]-1, 'pilot_loc' : np.r_[-21, -7, 7, 21] + 32, 'CPlen':16}
+ofdm64dict_noguardband={ 'framelen':64,'data_loc': np.r_[-32:-22+1, -20:-8+1, 
+    -6:-1+1, 1:6+1, 8:20+1, 22:31+1 ], 'pilot_loc' : np.r_[-21, -7, 7, 21], 'CPlen':16}
 
 #constant pilot sequences from the standard
-PLPCsyn_short=np.sqrt(16/6)*np.r_[0,0,1+1j,0,0,0,-1-1j,0,0,0,1+1j,0,0,0,-1-1j,0,0,0,-1-1j,0,0,0,1+1j,0,0,0,0,0,0,0,-1-1j,0,0,0,-1-1j,0,0,0,1+1j,0,0,0,1+1j,0,0,0,1+1j,0,0]
+#Standard presentation: neq freq=0  #-32               -24        -20         -16         -12         -8         -4                  4           8         12         16          20         24              
+PLPCsyn_short=np.sqrt(13/6)*np.array([0,0,0,0,0,0,0,0,1+1j,0,0,0,-1-1j,0,0,0,1+1j,0,0,0,-1-1j,0,0,0,-1-1j,0,0,0,1+1j,0,0,0,0,0,0,0,-1-1j,0,0,0,-1-1j,0,0,0,1+1j,0,0,0,1+1j,0,0,0,1+1j,0,0,0,1+1j,0,0,0,0,0,0,0],ndmin=2).T
 
-PLPCsyn_long=np.r_[1,1,-1,-1,1,1,-1,1,-1,1,1,1,1,1,1,-1,-1,1,1,-1,1,-1,1,1,1,1,1,0,1,-1,-1,1,1,-1,1,-1,1,-1,-1,-1,-1,-1,1,1,-1,-1,1,-1,1,-1,1,1,1,1]
+#Standard presentation-32              -24      -20       -16     -12       -8        -4                 4         8         12         16        20        24              
+PLPCsyn_long=np.array([0,0,0,0,0,0,1,1,-1,-1,1,1,-1,1,-1,1,1,1,1,1,1,-1,-1,1,1,-1,1,-1,1,1,1,1,0,1,-1,-1,1,1,-1,1,-1,1,-1,-1,-1,-1,-1,1,1,-1,-1,1,-1,1,-1,1,1,1,1,0,0,0,0,0],ndmin=2).T
+#Convert to "Math" IFFT/FTT format: to_fft=symbol_in_802_standard_format[Freqmap], maps the negative frequencies to the upper half
+
+#These are identical, lower works only in python
+#Freqmap=np.r_[np.arange(32,64), np.arange(0,32)] #Maps negative frequencies to the end of the array
+Freqmap=np.r_[np.arange(-32,0), np.arange(0,32)] #Maps negative frequencies to the end of the array
+
 
 bbsigdict_ofdm_sinusoid3={ 'mode':'ofdm_sinusoid', 'freqs':[1.0e6 , 13e6, 17e6 ], 'length':2**14, 'BBRs':20e6 };
 
@@ -31,7 +44,7 @@ bbsigdict_ofdm_sinusoid3={ 'mode':'ofdm_sinusoid', 'freqs':[1.0e6 , 13e6, 17e6 ]
 bbsigdict_randombitstream_QAM4_OFDM={ 'mode':'ofdm_random_qam', 'QAM':4, 'length':2**14, 'BBRs': 20e6 };
 
 
-class signal_generator_802_11n(rtl,thesdk):
+class signal_generator_802_11n(thesdk):
 
     #802.11 OFDM Dictionaries define the structure of the OFDM frames
     def __init__(self,*arg): 
@@ -45,6 +58,7 @@ class signal_generator_802_11n(rtl,thesdk):
         self.model='py';                         #can be set externally, but is not propagated
         self._Z = refptr();
         self._classfile=__file__
+        self.DEBUG= True
 
         if len(arg)>=1:
             parent=arg[0]
@@ -54,8 +68,7 @@ class signal_generator_802_11n(rtl,thesdk):
         pass
 
     def ofdm_sinusoid(self):
-        ofdmdict=self.ofdmdict
-
+        ofdmdict=ofdm64dict_noguardband
         bbsigdict=self.bbsigdict
         framelen=ofdmdict['framelen']
         length=bbsigdict['length']
@@ -64,12 +77,22 @@ class signal_generator_802_11n(rtl,thesdk):
         CPlen=ofdmdict['CPlen']
         #QAM=bbsigdict['QAM']
         BBRs=bbsigdict['BBRs']
-        signalindexes=np.round(np.array(self.bbsigdict['freqs'])/self.Rs*framelen).astype(int)
+        
+        #This defines locations as in 802 standard index 0 equals frequency -32=-fs/2
+        signalfrequencies=np.round(np.array(self.bbsigdict['freqs'])/(BBRs/2)*framelen/2).astype(int)
+        print(signalfrequencies)
         frame=np.zeros((frames,framelen))
-        frame[:,signalindexes]=1
-        datasymbols=frame[:,ofdmdict['data_loc']]
-        pilotsymbols=frame[:,ofdmdict['pilot_loc']]
-        interpolated=mdm.ofdmMod(ofdmdict,datasymbols,pilotsymbols)
+        frame[:,signalfrequencies+32]=1
+        print(frame[0,:])
+        #Need to map onece in order to get remapped right during modulation
+        datastream=frame[:,ofdmdict['data_loc']+32]
+        pilotstream=frame[:,ofdmdict['pilot_loc']+32]
+        
+        print(datastream[0,:])
+        print(pilotstream[0,:])
+
+        #Modulator takes care of mapping the negative frequencises to the end of the array
+        interpolated=mdm.ofdmMod(ofdmdict,datastream,pilotstream)
         interpolated=interpolated.reshape((-1,(framelen+CPlen)))
         interpolated=self.interpolate_at_ofdm({'signal':interpolated})
         length=interpolated.shape[1]
@@ -122,13 +145,13 @@ class signal_generator_802_11n(rtl,thesdk):
                 wordstream, qamsignal[i]= mdm.qamModulateBitStream(bitstream[i], QAM) #Modulated signal per user
                 qamsignal[i]=qamsignal[i].reshape((1,qamsignal.shape[1]))
                 frame= qamsignal[i].reshape((-1,framelen)) #The OFDM frames
-                datasymbols=frame[:,ofdmdict['data_loc']]   #Set the data
-                pilotsymbols=frame[:,ofdmdict['pilot_loc']] #In this case, also pilot carry bits
-                if (factor({'n':self.Rs/BBRs}))[0] != 2:
+                dataframe=frame[:,ofdmdict['data_loc']+32]   #Set the data
+                pilotframe=frame[:,ofdmdict['pilot_loc']+32] #In this case, also pilot carry bits
+                if (mr.factor({'n':self.Rs/BBRs}))[0] != 2:
                     print("SHIT ALERT: %s: The first interpolation factor for 802.11n is more than 2 \n and I'am too lazy to implement dedicated filter mask" %(self.__class__.__name__))
                     quit()
 
-                interpolated=mdm.ofdmMod(ofdmdict,datasymbols,pilotsymbols) #Variable for interpolation
+                interpolated=mdm.ofdmMod(ofdmdict,dataframe,pilotframe) #Variable for interpolation
                 interpolated=interpolated.reshape((-1,(framelen+CPlen)))
                 interpolated=self.interpolate_at_ofdm({'signal':interpolated})
                 length=interpolated.shape[1]
@@ -159,13 +182,63 @@ class signal_generator_802_11n(rtl,thesdk):
                     usersig[i,:,:]=np.transpose(np.ones((self.Txantennas,1))@chained)
                 
             self._Z.Value=usersig 
+    
+    def gen_plpc_preamble_field(self):
+        #Need to generate the IFFT according to stardard
+        #t=np.arange(64)
+        #t.shape=(-1,1)
+        #k=np.arange(-32,32)
+        #k.shape=(-1,1)
+        #IFFTmat=1/64*np.exp(2j*np.pi*t@k.T/64)
+        #seq_short=IFFTmat@PLPCsyn_short
+        
+        #Equivalent method using the zero  indexing FFT
+        #Python sucks. Be careful with dimensions
+        #shift=-32  #Negative frequency shift by Fs/2
+        #fshift=np.r_[PLPCsyn_short[shift::], PLPCsyn_short[0:shift]]
+        #print(Freqmap)
+        #fshift=PLPCsyn_short[Freqmap]
+        #seq_short2=np.fft.ifft(PLPCsyn_short[Freqmap2],axis=0)
+        #print(seq_short-seq_short2)
+
+        #Sequence in time domain
+        seq_short=np.fft.ifft(PLPCsyn_short[Freqmap],axis=0)
+        #extend to 161 samples
+        seq_short_extended=np.array([], ndmin=2,dtype='complex')
+        for i in range(4):
+            seq_short_extended=np.r_['1', seq_short_extended, seq_short.T]
+
+        ##windowing
+        win=np.r_[0.5, np.ones((159)), 0.5]
+        self.PLPCseq_short=seq_short_extended[0,0:161]*win
+        self.PLPCseq_short.shape=(-1,1)
+        #print(np.fft.fft(seq_short,axis=0))
+
+        ##Generate long sequence
+        seq_long=np.fft.ifft(PLPCsyn_long[Freqmap],axis=0)
+        seq_long_extended=np.array([], ndmin=2,dtype='complex')
+        print(seq_long)
+        for i in range(4):
+            seq_long_extended=np.r_['1', seq_long_extended, seq_long.T]
+
+        print(seq_long_extended)
+        self.PLPCseq_long=seq_long_extended[0,0:161]*win
+        self.PLPCseq_long.shape=(-1,1)
+
+        a=np.r_['0',self.PLPCseq_short, np.zeros((self.PLPCseq_long.shape[0]-1,1))] 
+        b=np.r_['0', np.zeros((self.PLPCseq_short.shape[0]-1,1)), self.PLPCseq_long]
+        self.PLPCseq=a+b 
+        print(self.PLPCseq)
+ 
+    def gen_plcp_header_field(self):
+        pass
 
     def interpolate_at_ofdm(self,argdict={'signal':[]}):
         ratio=self.Rs/self.bbsigdict['BBRs']
         signal=argdict['signal']
         #Currently fixeed interpolation. check the fucntion definitions for details
-        factors=factor({'n':ratio})
-        filterlist=self.generate_interpolation_filterlist({'interp_factor':ratio})
+        factors=mr.factor({'n':ratio})
+        filterlist=mr.generate_interpolation_filterlist({'interp_factor':ratio})
         msg="Signal length is now %i" %(signal.shape[1])
         self.print_log({'type':'I', 'msg': msg}) 
         #This is to enable growth of the signal length that better mimics the hardware
@@ -182,88 +255,17 @@ class signal_generator_802_11n(rtl,thesdk):
                 signali[symbol,:]=t
             else:
                 signali[symbol,:]=t
-        print("Signal length is now %i" %(signali.shape[1]))
-        self._filterlist=filterlist
+        msg="Signal length is now %i" %(signal.shape[1])
+        self.print_log({'type':'I', 'msg': msg}) 
         #print(filterlist)
         return signali
 
-    def generate_interpolation_filterlist(self,argdict={'interp_factor':1}):
-        #Use argument dictionary. Makes modifications easier.
-        interp_factor=argdict['interp_factor']
-        
-        attenuation=70 #Desired attenuation in decibels
-        factors=factor({'n':interp_factor})
-        #print(factors)
-        fsample=1
-        BW=0.45
-        numtaps=65     # TAps for the first filterThis should be somehow verified
-        #Harris rule. This is to control stability of Rmez
-        #numtaps= int(np.ceil(attenuation*fsample*factors[0]/(fsample/2-BW)))    # TAps for the first filterThis should be somehow verified
-        desired=np.array([ 1, 10**(-attenuation/10)] )
-        #check the mask specs from standard 
-        #mask=np.array([ 1, 10**(-28/10)    ] )
-        filterlist=list()
-        if interp_factor >1:
-            for i in factors:
-                fact=i
-                #print(fsample)
-                if  fsample/(0.5)<= 8: #FIR is needed
-                    msg= "BW to sample rate ratio is now %s" %(fsample/0.5)
-                    self.print_log({'type': 'I', 'msg':msg })
-                    msg="Interpolation by %i" %(fact)
-                    self.print_log({'type': 'I', 'msg':msg })
-                    bands=np.array([0, BW, (fsample*fact/2-BW), fact*fsample/2])
-                    filterlist.append(sig.remez(numtaps, bands, desired, Hz=fact*fsample))
-                    fsample=fsample*fact #increase the sample frequency
-                    numtaps=np.amax([3, int(np.floor(numtaps/fact)) + int((np.floor(numtaps/fact)%2-1))]) 
-                else:
-                    print("BW to sample rate ratio is now %s" %(fsample/0.5))
-                    fact=fnc.reduce(lambda x,y:x*y,factors)/fsample
-                    print("Interpolation with 3-stage CIC-filter by %i" %(fact))
-                    fircoeffs=np.ones(int(fact))/(fact) #do the rest of the interpolation with 3-stage CIC-filter
-                    fircoeffs=fnc.reduce(lambda x,y: np.convolve(x,y),list([fircoeffs, fircoeffs, fircoeffs]))
-                    filterlist.append(fircoeffs)
-                    #print(filterlist)
-                    fsample=fsample*fact #increase the sample frequency
-                    print("BW to sample rate ratio is now %s" %(fsample/0.5))
-                    break
-        else:
-            print("Interpolation ratio is 1. Generated unit coefficient")
-            filterlist.append([1.0]) #Ensure correct operation in unexpected situations.
-        return filterlist
 
-    #def generate_802_11n_plpc(self):
-    #    ofdmdict=self.ofdmdict
-    #    framelen=ofdmdict['framelen']
-    #    CPlen=ofdmdict['CPlen']
-    #    QAMshort=4
-    #    QAMlong=2
-    #    BBRs=bbsigdict['BBRs']
-    #    #The length is approx this many frames
-    #    
-    #    #Generate random bitstreams per user
-    #    #bitstream(user,time,antenna)
-    #    #sequence_short=[] 
-    #    #for in in range(10)
-    #    symbol_short=np.r[np.zeros((1,6)), PLPCsyn_short, np.zeros((1,6)))] #These are already 4-QAM modulated
-    #    seq_short=np.ones((10,1))@symbol_short #10 short symbols
-
-    #    symbol_long=np.r[np.zeros((1,6)), PLPCsyn_long, np.zeros((1,6)))]   #These are 2-QAM modulated
-    #    seq_long=np.ones((2,1))@symbol_long                                 #2 long symbols
-    #    
-    #    #Of course windowing consists anomalies.Performed after IFFT anyway
-    #    symbols=np.r('0',seq_short,seq_long)
-    #    datasymbols=symbols[:,ofdmdict['data_loc']]   #Set the data
-    #    pilotsymbols=symbols[:,ofdmdict['pilot_loc']] #Set the pilots
-    #    modulated=mdm.ofdmMod(ofdmdict,datasymbols,pilotsymbols) #Variable for interpolation
-
-
-    #    #Init the qam signal, frame and out
-    #    #qamsignal is different for the users, but initially identical for the TXantennas 
-    #    frame=np.zeros((int(frames),int(framelen)),dtype='complex')
 
 #Window to taper OFDM symbols
 def window(argdict={'Tr':100e-9, 'length':478, 'fs':80e6, 'duration':240 }):
+    #Length is the length of the signal. Duration is the duration of the payload data
+    #Tails are due to interpolation
     #Tr is the window rise/fall time 100ns is from the specification
     Tr=argdict['Tr']
     fs=argdict['fs']
@@ -282,34 +284,27 @@ def window(argdict={'Tr':100e-9, 'length':478, 'fs':80e6, 'duration':240 }):
          window[i]=np.sin(np.pi/2*(0.5-(t[i]-T)/Tr))**2
     return window
 
-    def run(self,*arg):
-        if len(arg)>0:
-            par=True      #flag for parallel processing
-            queue=arg[0]  #multiprocessing.Queue as the first argument
-        else:
-            par=False
+#    def run(self,*arg):
+#        if len(arg)>0:
+#            par=True      #flag for parallel processing
+#            queue=arg[0]  #multiprocessing.Queue as the first argument
+#        else:
+#            par=False
+#
+#        if self.model=='py':
+#            out=np.array(self.iptr_A.Value)
+#            if par:
+#                queue.put(out)
+#            self._Z.Value=out
 
-        if self.model=='py':
-            out=np.array(self.iptr_A.Value)
-            if par:
-                queue.put(out)
-            self._Z.Value=out
 
-def factor(argdict={'n':1}):
-    #This is a function to calculate factors of an integer as in Matlab
-    # "Everything in Matlab is available in Python" =False.
-    reminder=argdict['n']
-    factors=np.array([])
-    while reminder >1:
-        notfound=True
-        for i in range(2,int(np.sqrt(reminder)+2)):
-            if reminder % i == 0:
-                factors=np.r_[ factors, i]
-                reminder=reminder/i
-                notfound=False
-                break
-        if notfound==True:
-                factors=np.r_[ factors, reminder]
-                reminder=0
-    return factors
+if __name__=="__main__":
+    import scipy as sci
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from refptr import *
+    from signal_generator_802_11n import *
+    t=signal_generator_802_11n()
+    t.gen_plpc_preamble_field()
+
 
